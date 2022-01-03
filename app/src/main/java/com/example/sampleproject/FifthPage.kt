cdbd24cfba97e98f21793780b180687ef94232e7
Rishi <apprishiategood002@gmail.com>
2022-01-04 00:09:03 +0530

    Created calendar dialog box to choose the day of event

--- app/src/main/java/com/example/sampleproject/FifthPage.kt
+++ app/src/main/java/com/example/sampleproject/FifthPage.kt
@@ -1,11 +1,47 @@
 package com.example.sampleproject
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FifthPage : AppCompatActivity() {
+
+    private lateinit var dateText : TextView
+    private lateinit var dateButton : Button
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth_page)
+
+        dateButton = findViewById(R.id.button8)
+        dateText = findViewById(R.id.textView3)
+
+        val myCalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateLable(myCalendar)
+        }
+
+        dateButton.setOnClickListener {
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+
+    private fun updateLable(myCalendar: Calendar) {
+        val myFormat = "dd-mm-yyyy"
+        val sdf = SimpleDateFormat(myFormat,Locale.UK)
+        dateText.setText(sdf.format(myCalendar.time))
     }
 }

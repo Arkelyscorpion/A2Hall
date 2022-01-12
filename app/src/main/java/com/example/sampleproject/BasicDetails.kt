c5d06255cfde920174977b08965205f73c1a4bed
rishiyanth <apprishiategood002@gmail.com>
2022-01-13 00:09:12 +0530

    Disabled the "Next" buttons 2.0

--- app/src/main/java/com/example/sampleproject/BasicDetails.kt
+++ app/src/main/java/com/example/sampleproject/BasicDetails.kt
@@ -3,9 +3,12 @@ package com.example.sampleproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import com.example.sampleproject.databinding.ActivityMainBinding
 
 class BasicDetails : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_details)
@@ -18,7 +21,7 @@ class BasicDetails : AppCompatActivity() {
 
         val nextButton = findViewById<Button>(R.id.button2)
         nextButton?.setOnClickListener() {
-            val intent = Intent(this,SecondaryDetails::class.java)
+            val intent = Intent(this, SecondaryDetails::class.java)
             startActivity(intent)
         }
     }
